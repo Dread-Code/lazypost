2a796e4dddc72875d77dae099679e48838e06aa9
Lucas Ospina Aristizabal <lucasospina97@gmail.com>
2026-08-19 20:26:25 -0500

    chore(site): match star badge to dracula accent

--- site/src/data/site.js
+++ site/src/data/site.js
@@ -1,4 +1,4 @@
 export const github = 'https://github.com/Dread-Code/lazypost';
 export const starsBadge =
-  'https://img.shields.io/github/stars/Dread-Code/lazypost.svg?style=flat&label=stars&color=7ec699';
+  'https://img.shields.io/github/stars/Dread-Code/lazypost.svg?style=flat&label=stars&color=bd93f9';
 export const installUrl = 'https://raw.githubusercontent.com/Dread-Code/lazypost/main/install.sh';
