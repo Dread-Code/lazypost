5b8068bc715a8cec43ab98118442c0e06cd5edba
Lucas Ospina Aristizabal <lucasospina97@gmail.com>
2026-08-19 21:52:40 -0500

    feat(site): docs layout, swatches, table styles

---
+++ site/src/data/docs.js
@@ -0,0 +1,11 @@
+export const docs = [
+  { href: 'docs/quickstart', title: 'Quick start' },
+  { href: 'docs/installation', title: 'Installation' },
+  { href: 'docs/keybindings', title: 'Keybindings' },
+  { href: 'docs/collections', title: 'Collections' },
+  { href: 'docs/environments', title: 'Environments' },
+  { href: 'docs/scripting', title: 'Scripting' },
+  { href: 'docs/importers', title: 'Importers' },
+  { href: 'docs/themes', title: 'Themes' },
+  { href: 'docs/faq', title: 'FAQ' },
+];
